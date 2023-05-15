52fb4c1ea8ef54949fc1711916f373a15723bce7
ycy <cksdyd93@gmail.com>
2023-05-15 11:19:24 +0900

    23-05-15 2교시

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,33 +1,54 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Counter from '../components/Counter';
-import { decrease, increase } from '../modules/counter';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import Counter from '../components/Counter';
+// import { decrease, increase } from '../modules/counter';
 
 
-const CounterContainer = ({number, increase, decrease}) => {
+// const CounterContainer = ({number, increase, decrease}) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+
+// const mapStateToProps = (state) => ({ number: state.counter.number });
+// // 1번방법
+// const mapDispatchToProps = {increase, decrease};
+
+// // 2번방법
+//   // bindActionCreators({increase, decrease}, dispatch);
+  
+// // 3번방법
+//   // {
+//   // increase: () => {
+//   //   // console.log("increase");
+//   //   dispatch(increase());
+//   // },
+//   // decrease: () => {
+//   //   // console.log("decrease");
+//   //   dispatch(decrease());
+//   // },
+// // }
+
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Counter, {} from '../components/Counter'
+import { decrease, increase } from '../modules/counter'
+
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback (() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback (() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter 
+      number={number} 
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+    />
   );
 };
 
-const mapStateToProps = (state) => ({ number: state.counter.number });
-// 1번방법
-const mapDispatchToProps = {increase, decrease};
-
-// 2번방법
-  // bindActionCreators({increase, decrease}, dispatch);
-  
-// 3번방법
-  // {
-  // increase: () => {
-  //   // console.log("increase");
-  //   dispatch(increase());
-  // },
-  // decrease: () => {
-  //   // console.log("decrease");
-  //   dispatch(decrease());
-  // },
-// }
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default CounterContainer;
--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,37 +1,68 @@
-import React from 'react';
-import Todos from '../components/Todos';
-import { connect } from 'react-redux';
-import { changeInput, insert, toggle, remove } from '../modules/todos'
+// import React from 'react';
+// import Todos from '../components/Todos';
+// import { connect } from 'react-redux';
+// import { changeInput, insert, toggle, remove } from '../modules/todos'
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+// const TodosContainer = ({
+//   input,
+//   todos,
+//   changeInput,
+//   insert,
+//   toggle,
+//   remove,
+// }) => {
+//   return (
+//     <Todos 
+//       input={input}
+//       todos={todos}
+//       onChangeInput={changeInput}
+//       onInsert={insert}
+//       onToggle={toggle}
+//       onRemove={remove}
+//     />
+//   );
+// };
+
+// export default connect(
+//   ({todos}) => ({
+//     input: todos.input,
+//     todos: todos.todos
+//   }),
+//   {
+//     changeInput,
+//     insert,
+//     toggle,
+//     remove,
+//   },
+// )(TodosContainer);
+
+import React, { useCallback } from 'react';
+import Todos from '../components/Todos';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeInput, insert, toggle, remove } from '../modules/todos';
+
+const TodosContainer = () => {
+  const {input, todos} = useSelector(({ todos })=>({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
+  const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
   return (
-    <Todos 
-      input={input}
-      todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
-    />
+    <div>
+      <Todos 
+        input={input}
+        todos={todos}
+        onChangeInput={onChangeInput}
+        onInsert={onInsert}
+        onToggle={onToggle}
+        onRemove={onRemove}
+      />
+    </div>
   );
 };
 
-export default connect(
-  ({todos}) => ({
-    input: todos.input,
-    todos: todos.todos
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  },
-)(TodosContainer);
+export default React.memo(TodosContainer);
